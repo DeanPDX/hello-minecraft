e46cc0abe32919e5ba8710338767b956d6ef1110
Dean Davidson <ddavidson85@gmail.com>
2015-05-10 12:52:02 -0700

    Added explosion when activated with flint and steel.

--- Hello Minecraft/src/main/java/com/wimpy-tnt/WimpyTNTBlock/WimpyTNTBlock.java
+++ Hello Minecraft/src/main/java/com/wimpy-tnt/WimpyTNTBlock/WimpyTNTBlock.java
@@ -1,9 +1,21 @@
 package hellominecraft.wimpytntmod;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockTNT;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraftforge.fml.common.registry.GameRegistry;
+import net.minecraft.world.World;
+import net.minecraft.world.Explosion;
+import net.minecraft.util.BlockPos;
+import net.minecraft.block.state.BlockState;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.init.Items;
+import net.minecraft.client.Minecraft;
+
 
 public class WimpyTNTBlock extends Block 
 {
@@ -21,5 +33,28 @@ public class WimpyTNTBlock extends Block
 	{
 		return name;
 	}
+
+	public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ)
+    {
+        if (playerIn.getCurrentEquippedItem() != null)
+        {
+            Item item = playerIn.getCurrentEquippedItem().getItem();
+
+            if (item == Items.flint_and_steel || item == Items.fire_charge)
+            {
+				float f = .5F;
+
+				World worldObj = Minecraft.getMinecraft().theWorld;
+				
+				worldObj.createExplosion(null, pos.getX(),
+					pos.getY(), pos.getZ(), f, true);
+
+				worldIn.setBlockToAir(pos);
+                return true;
+            }
+        }
+
+        return super.onBlockActivated(worldIn, pos, state, playerIn, side, hitX, hitY, hitZ);
+    }
 
 }
